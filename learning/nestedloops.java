e52aa621ed9d1e315955604f8612c0151092a803
vroomvroomvoom <AWOLF@LSHIGH.ORG>
2022-11-02 09:56:33 -0700

    nested loops code

---
+++ learning/nestedloops.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class nestedloops {
+    public static void main(String[] args){
+
+        int x = 5;
+        int y = 0;
+
+        for(y=0; y<5; y++){
+            for(x=5; x>y; x--){
+                System.out.print("henry");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
